fac1823121320b61b42ece5e73fc16ef83f99692
damorest <pptreiding@gmail.com>
2023-08-25 13:17:02 +0300

    post Model

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -1,5 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 
 class CreatePostScreen extends StatefulWidget {
 
@@ -12,8 +14,53 @@ class CreatePostScreen extends StatefulWidget {
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
 
+  String _description = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    final File imageFile = ModalRoute.of(context)!.settings.arguments as File;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Create Post")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 1/4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: FileImage(imageFile),
+                fit: BoxFit.cover
+              )
+            ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter a description",
+              ),
+              textInputAction: TextInputAction.done,
+              //Limits number of characters
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(150),
+              ],
+
+              onChanged: (value) {
+                _description = value;
+              },
+
+              onEditingComplete: () {
+                //TODO:- Write ti Firebase, pass image
+                _submit(image: imageFile, description: _description);
+              },
+            )
+
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/posts_screen.dart
+++ lib/screens/posts_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/bloc/auth_cubit.dart';
+import 'package:chat_app/screens/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
@@ -29,15 +30,19 @@ class _PostsScreenState extends State<PostsScreen> {
               if(xFile != null) {
                 final File file = File(xFile.path);
 
-                Navigator.of(context).pushNamed(CreatePostScreen.id);
+                Navigator.of(context).pushNamed(CreatePostScreen.id, arguments: file);
               }
             });
 
           }, icon: const Icon(Icons.add)),
 
           IconButton(onPressed: () {
-            context.read<AuthCubit>().signOut();
-          }, icon: Icon(Icons.logout)),
+            context.read<AuthCubit>().signOut().then((_) =>
+            Navigator.of(context).
+            pushReplacementNamed(SignInScreen.id));
+
+          },
+              icon: Icon(Icons.logout)),
         ],
       ),
       // Якщо в body  ретурнити контейнер, то все працює, а з ЛістБілдером, викидає, так як Ліст Білдер пустий.)
