e74f1b6f19d9c1c533e85e5dc4916342c315d5ac
damorest <pptreiding@gmail.com>
2023-08-25 03:08:20 +0300

    create post screen. Crash

---
+++ lib/screens/create_post_screen.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class CreatePostScreen extends StatefulWidget {
+
+  static const String id = "create_post_screen";
+  const CreatePostScreen({super.key});
+
+  @override
+  State<CreatePostScreen> createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
